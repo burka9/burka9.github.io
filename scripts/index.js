8eda9beb427751c61d9142bbbb8473bc8563cda7
Biruk Ephrem <birukeph@gmail.com>
2022-09-16 13:30:07 +0300

    modified contents and added downloadable resume

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 const d = document
 const w = window
 
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    rel: 'noopener noreferrer',
+    href: href,
+  }).click();
+}
+
 let prevScroll = 0
 let scrollDown = true
 let screen = {
@@ -96,6 +104,89 @@ const changeText = () => {
 	}
 }
 
+const download_cv = () => {
+	openInNewTab('/docs/resume.pdf')
+}
+
+const educations = [
+	{
+		year: '2016 - 2021',
+		title: 'Bachelor of Science in Computer Engineering',
+		subtitle: 'Addis Ababa Science and Technology University',
+		// description: 'A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.',
+	},
+	{
+		year: '2015 - 2002',
+		title: 'Primary and Secondary School',
+		subtitle: 'Ethio-Parents\' School',
+	}
+]
+
+let education_container = d.querySelector('#education')
+const loadEducation = () => {
+	educations.forEach(e => {
+		let education = d.createElement('div')
+		education.classList.add('item')
+		let year = d.createElement('p')
+		year.classList.add('year')
+		year.innerText = e.year
+		let title = d.createElement('h2')
+		title.classList.add('title')
+		title.innerText = e.title
+		let subtitle = d.createElement('h3')
+		subtitle.classList.add('subtitle')
+		subtitle.innerText = e.subtitle
+		let description = d.createElement('p')
+		description.classList.add('description')
+		description.innerText = e.description ? e.description : ''
+		education.append(...[year, title, subtitle, description])
+		education_container.appendChild(education)
+	})
+}
+
+const experiences = [
+	{
+		year: 'Nov 2020 - Apr 2012',
+		title: 'Game Developer',
+		subtitle: 'Desert Rose Consultancy',
+		description: 'Digitalizing an existing game for use on as a web game. Integrating web server with a bot accessible from Telegram messaging platform.	Hosting and testing games online and on local networks.'
+	},
+	{
+		year: 'Apr 2021 - Jun 2021',
+		title: 'Intern Web Developer',
+		subtitle: 'Desert Rose Consultancy',
+		description: 'Designed and executed production strategies for a multiplayer web game, and created UI/UX and backend implementations.'
+	},
+	{
+		year: 'December 2021 - Present',
+		title: 'Software and Web Developer',
+		subtitle: 'Netib Consult',
+		description: 'Managing and planning software development phases. Server management and hosting. Web development.'
+	}
+]
+
+let experience_container = d.querySelector('#experience')
+const loadExperience = () => {
+	experiences.forEach(e => {
+		let experience = d.createElement('div')
+		experience.classList.add('item')
+		let year = d.createElement('p')
+		year.classList.add('year')
+		year.innerText = e.year
+		let title = d.createElement('h2')
+		title.classList.add('title')
+		title.innerText = e.title
+		let subtitle = d.createElement('h3')
+		subtitle.classList.add('subtitle')
+		subtitle.innerText = e.subtitle
+		let description = d.createElement('p')
+		description.classList.add('description')
+		description.innerText = e.description ? e.description : ''
+		experience.append(...[year, title, subtitle, description])
+		experience_container.appendChild(experience)
+	})
+}
+
 const skills = [
 	{ name: 'HTML', skill: 92 },
 	{ name: 'CSS', skill: 86 },
@@ -132,12 +223,9 @@ const loadSkills = () => {
 }
 
 const services = [
-	{ name: 'Web Design', icon: 'icons/ux-design-svgrepo-com.svg', detail: 'A small river named Duden flows by their place and supplies it with the necessary regelialia.' },
-	{ name: 'Photography', icon: 'icons/camera-svgrepo-com.svg', detail: 'A small river named Duden flows by their place and supplies it with the necessary regelialia.' },
-	{ name: 'Web Development', icon: 'icons/web-development-svgrepo-com.svg', detail: 'A small river named Duden flows by their place and supplies it with the necessary regelialia.' },
-	{ name: 'Web Design', icon: 'icons/ux-design-svgrepo-com.svg', detail: 'A small river named Duden flows by their place and supplies it with the necessary regelialia.' },
-	{ name: 'Photography', icon: 'icons/camera-svgrepo-com.svg', detail: 'A small river named Duden flows by their place and supplies it with the necessary regelialia.' },
-	{ name: 'Web Development', icon: 'icons/web-development-svgrepo-com.svg', detail: 'A small river named Duden flows by their place and supplies it with the necessary regelialia.' },
+	{ name: 'Web Design', icon: 'icons/ux-design-svgrepo-com.svg', detail: 'Plan, create and code internet sites and web pages. Creating the best design and layouts for a web site.' },
+	{ name: 'Photography', icon: 'icons/camera-svgrepo-com.svg', detail: 'Capture moments in a commercial quality using experties, quality and composition skills.' },
+	{ name: 'Software Development', icon: 'icons/web-development-svgrepo-com.svg', detail: 'Design, program, build, deploy and maintain software using many different skills and tools.' },
 ]
 
 let service_container = d.querySelector('.services .list')
@@ -167,15 +255,9 @@ const loadServices = () => {
 
 
 const projects = [
-	{ photo: 'test', title: 'test', type: 'test' },
-	{ photo: 'test', title: 'test', type: 'test' },
-	{ photo: 'test', title: 'test', type: 'test' },
-	{ photo: 'test', title: 'test', type: 'test' },
-	{ photo: 'test', title: 'test', type: 'test' },
-	{ photo: 'test', title: 'test', type: 'test' },
-	{ photo: 'test', title: 'test', type: 'test' },
-	{ photo: 'test', title: 'test', type: 'test' },
-	{ photo: 'test', title: 'test', type: 'test' },
+	{ photo: 'luna_home.png', title: 'Luna Group', type: 'Web Development', link: 'https://bot.netibconsult.com/' },
+	{ photo: 'yoni_movies.png', title: 'Yoni Entertainment', type: 'Web Development', link: 'https://yoni-movies.herokuapp.com/' },
+	{ photo: 'filmia.png', title: 'Filmia', type: 'Game Development', link: 'http://filmia.herokuapp.com/' },
 ]
 
 let project_container = d.querySelector('.projects .list')
@@ -184,18 +266,19 @@ const loadProjects = () => {
 	projects.forEach((p, i) => {
 		let item = d.createElement('div')
 		item.classList.add('item')
-		item.style.backgroundImage = `url("imgs/project-${(i+4)%7 + 1}.jpg")`
+		item.style.backgroundImage = `url("imgs/${p.photo}")`
+		item.onclick = () => openInNewTab(p.link)
 
 		let div = d.createElement('div')
 		div.classList.add('background')
 
 		let title = d.createElement('h1')
 		title.classList.add('title')
-		title.innerHTML = 'Project ' + (i+1)
+		title.innerHTML = p.title
 		
 		let type = d.createElement('h2')
 		type.classList.add('type')
-		type.innerHTML = 'Project Type'
+		type.innerHTML = p.type
 
 		item.appendChild(div)
 		item.appendChild(title)
@@ -208,6 +291,8 @@ const loadProjects = () => {
 w.onload = () => {
 	w.addEventListener('scroll', onscroll)
 	setTimeout(() => deleteText(), change_delay)
+	loadEducation()
+	loadExperience()
 	loadSkills()
 	loadServices()
 	loadProjects()
